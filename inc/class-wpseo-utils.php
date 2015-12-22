0be1bc91c214b154ed98c6596286e2fa3e7783d4
Jip Moors <jip@gho.nl>
2015-12-22 12:30:38 +0100

    Changed get_instance WPSEO_Options_Title to a retrieval via WPSEO_Options

--- inc/class-wpseo-utils.php
+++ inc/class-wpseo-utils.php
@@ -776,13 +776,17 @@ class WPSEO_Utils {
 		$replacement = WPSEO_Options::get_default( 'wpseo_titles', 'separator' );
 
 		// Get the titles option and the separator options.
-		$titles_options    = get_option( 'wpseo_titles' );
-		$seperator_options = WPSEO_Option_Titles::get_instance()->get_separator_options();
+		$titles_options = get_option( 'wpseo_titles' );
 
-		// This should always be set, but just to be sure.
-		if ( isset( $seperator_options[ $titles_options['separator'] ] ) ) {
-			// Set the new replacement.
-			$replacement = $seperator_options[ $titles_options['separator'] ];
+		$wpseo_titles = WPSEO_Options::get_option_instance( 'wpseo_titles' );
+		if ( $wpseo_titles instanceof WPSEO_Option_Titles ) {
+			$seperator_options = $wpseo_titles->get_seperator_options();
+
+			// This should always be set, but just to be sure.
+			if ( isset( $seperator_options[ $titles_options['separator'] ] ) ) {
+				// Set the new replacement.
+				$replacement = $seperator_options[ $titles_options['separator'] ];
+			}
 		}
 
 		/**
